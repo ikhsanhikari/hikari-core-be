9f11da0d0c2f1573396417d245abdb3d6f012a2b
Nurikhsan <ikhsanhikari29@gmail.com>
2019-04-29 19:44:19 +0700

    init

---
+++ src/main/java/id/hikari/core/be/hikaricorebe/HikariCoreBeApplication.java
@@ -0,0 +1,13 @@
+package id.hikari.core.be.hikaricorebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariCoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariCoreBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/hikari/core/be/hikaricorebe/HikariCoreBeApplicationTests.java
@@ -0,0 +1,16 @@
+package id.hikari.core.be.hikaricorebe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HikariCoreBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
